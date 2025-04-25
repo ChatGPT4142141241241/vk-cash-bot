7e9601f374b0a0c1fbed513eae6f4645ddd65dbd
ChatGPT4142141241241 <fablbautista124@gmail.com>
2025-04-25 21:53:00 +1100

    Add files via upload

--- main.py
+++ main.py
@@ -78,5 +78,5 @@ def webhook():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get("PORT", 5000))
     server.run(host="0.0.0.0", port=port)
